6e298db4e3f1f9f674cb874a74d70fe996dd192d
Aldo Wiranata <148690939+alwiranata@users.noreply.github.com>
2025-05-13 22:42:00 +0700

    file : add async.js

--- js/async.js
+++ js/async.js
@@ -1,14 +1,60 @@
-//callbacks
 
+//callbacks
 const profile = (name, callbacks) => {
 	console.log("Name  : ", name)
-	callbacks
+	callbacks(19)
 }
 
-const old = (old) =>{
-    console.log("Old : ", old )
+const old = (old) => {
+	console.log("Old : ", old)
 }
 
 const addName = "Aldo Wiranata"
-const addOld = old(19) 
-profile(addName  , addOld)
+profile(addName, old)
+
+//promise
+
+const bootcamp = new Promise((resolve, reject) => {
+	const data = false
+	if (data) {
+		resolve("Lulus Bootcamp")
+	} else {
+		reject("Ulangi bootcamp")
+	}
+})
+
+bootcamp
+	.then((pesan) => console.log(pesan))
+	.catch((error) => console.error(error))
+
+//Async / Await
+
+const delay = (ms) => {
+	return new Promise((resolve) => {
+		console.log(typeof resolve)
+		setTimeout(resolve, ms)
+	})
+}
+
+const prosesBelajar = async () => {
+	console.log("Mulai Belajar JS")
+	await delay(5000)
+	console.log("Belajar JS ...")
+	await delay(4000)
+	console.log("Selesai Belajar JS")
+}
+
+prosesBelajar()
+
+//contoh asli Async / Await
+
+const getData = async () => {
+	try {
+		const res = await fetch("https://jsonplaceholder.typicode.com/users")
+		const data = await res.json()
+		console.log(data)
+	} catch (error) {
+		console.log(error)
+	}
+}
+getData()
