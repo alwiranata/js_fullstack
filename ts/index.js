011e6d2524788afdc0f75bf5a6c4ebc71c45e45d
Aldo Wiranata <148690939+alwiranata@users.noreply.github.com>
2025-05-18 18:25:20 +0700

    folder : update ts

--- ts/index.js
+++ ts/index.js
@@ -18,10 +18,70 @@ var Status;
 var profile1 = {
     id: 1,
     name: "Aldo Wiranata",
-    active: true
+    active: true,
 };
 var profile = "admin";
 var Active = Status.Approved;
 console.log(profile1);
 console.log(profile);
 console.log(Active);
+var data = {
+    nim: 2455201036,
+    nama: "Aldo Wiranata",
+    prodi: "TI",
+};
+console.log(data);
+//union
+var n = 20;
+n = "aldo";
+console.log(n);
+var add = function (pw) {
+    return pw;
+};
+console.log(add("12345678"));
+//tupple
+var value = ["aldo", 20];
+value = ["Wiranata", 19];
+console.log(value);
+var p = "Aldo";
+var d = "Wiranata";
+var isActived = true;
+var wrong = undefined;
+//enum
+var prodis;
+(function (prodis) {
+    prodis[prodis["TI"] = 0] = "TI";
+    prodis[prodis["TS"] = 1] = "TS";
+    prodis[prodis["ILKOM"] = 2] = "ILKOM";
+})(prodis || (prodis = {}));
+var role;
+(function (role) {
+    role["admin"] = "ADMIN";
+    role["user"] = "USER";
+})(role || (role = {}));
+var prodi = prodis.TI;
+var roles = role.admin;
+console.log(prodi);
+console.log(roles);
+//function
+var names = function (val) {
+    return "hello ".concat(val);
+};
+var getNames = "Aldo Wiranata";
+console.log(names(getNames));
+var persegiPanjang = function (p, l) {
+    return p * l;
+};
+var addPersegiPanjang = persegiPanjang(10, 10);
+console.log(addPersegiPanjang);
+var getLuasPersegiPanjang = function (n) {
+    return {
+        vLuas: n.l * n.p
+    };
+};
+var dataPersegiPanjang = {
+    p: 10,
+    l: 20
+};
+var getDataPersegiPanjang = getLuasPersegiPanjang(dataPersegiPanjang);
+console.log(getDataPersegiPanjang);
--- ts/index.ts
+++ ts/index.ts
@@ -1,42 +1,146 @@
-let nama : string = "aldo"
+let nama: string = "aldo"
 let angka: number = 12345678
-let isActive : boolean = true
+let isActive: boolean = true
 let datas: any = "aldo"
 datas = true
 datas = "hello"
 
-let values  :  number |  string = "123"
+let values: number | string = "123"
 values = 62895634889510
 
-let User : "admin"  | "user" = "admin"
-User ="user"
+let User: "admin" | "user" = "admin"
+User = "user"
 
-console.log(nama , angka , isActive , datas , values , User)
-
+console.log(nama, angka, isActive, datas, values, User)
 
 interface User {
-    id : number
-    name : string
-    active : boolean
+	id: number
+	name: string
+	active: boolean
 }
 
-type Role = "admin" | "Users" 
+type Role = "admin" | "Users"
 
 enum Status {
-    Pending,
-    Approved,
-    Rejected
+	Pending,
+	Approved,
+	Rejected,
 }
 
-const profile1 : User= {
-    id : 1,
-    name : "Aldo Wiranata",
-    active : true
+const profile1: User = {
+	id: 1,
+	name: "Aldo Wiranata",
+	active: true,
 }
 
-const profile : Role = "admin"
-const Active : Status = Status.Approved
+const profile: Role = "admin"
+const Active: Status = Status.Approved
 
 console.log(profile1)
 console.log(profile)
 console.log(Active)
+
+//interface
+interface Profile {
+	nim: number
+	nama: string
+}
+
+interface mahasiswa extends Profile {
+	prodi?: string
+}
+
+const data: mahasiswa = {
+	nim: 2455201036,
+	nama: "Aldo Wiranata",
+	prodi: "TI",
+}
+console.log(data)
+
+//union
+
+let n: string | number = 20
+n = "aldo"
+console.log(n)
+
+const add = (pw: string | number) => {
+	return pw
+}
+
+console.log(add("12345678"))
+
+//tupple
+
+let value: [string, number] = ["aldo", 20]
+value = ["Wiranata", 19]
+console.log(value)
+
+//type
+
+type p = string
+type d = string | number
+type v = boolean
+type err = null | undefined
+
+const p: p = "Aldo"
+const d: d = "Wiranata"
+const isActived: v = true
+const wrong: err = undefined
+
+//enum
+enum prodis {
+	TI,
+	TS,
+	ILKOM,
+}
+enum role {
+	admin = "ADMIN",
+	user = "USER",
+}
+
+const prodi: prodis = prodis.TI
+const roles: role = role.admin
+console.log(prodi)
+console.log(roles)
+
+//function
+const names = (val: string): string => {
+	return `hello ${val}`
+}
+
+type names = string
+const getNames: names = "Aldo Wiranata"
+
+console.log(names(getNames))
+
+const persegiPanjang = (p: number, l: number): number => {
+	return p * l
+}
+
+type tPersegiPanjang = number
+
+const addPersegiPanjang: tPersegiPanjang = persegiPanjang(10, 10)
+console.log(addPersegiPanjang)
+
+interface pPanjang {
+	p: number
+	l: number
+}
+
+interface luas{
+    vLuas  : number
+} 
+
+const getLuasPersegiPanjang = (n: pPanjang): luas => {
+	return  {
+      vLuas  :  n.l * n.p
+    }
+}
+
+const dataPersegiPanjang  ={
+    p : 10 ,
+    l : 20
+}
+
+const getDataPersegiPanjang =  getLuasPersegiPanjang(dataPersegiPanjang) 
+console.log(getDataPersegiPanjang.vLuas)
